f62a2da6d6a804280a86caaf2ac69b70d0d028cd
Jesse Newland <jesse@railsmachine.com>
2011-01-19 17:09:59 -0500

    better template

--- Templates/gen_server.tmTemplate/gen_server.erl
+++ Templates/gen_server.tmTemplate/gen_server.erl
@@ -1,63 +1,126 @@
--module(module).
--compile(export_all).
+%%%-------------------------------------------------------------------
+%%% @author John Doe <john@example.com>
+%%% @copyright (C) 2011, John Doe
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aaa).
 
 -behaviour(gen_server).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
-%% Public API
+%% API
+-export([start_link/0]).
 
-start() ->
-  gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
-stop(Module) ->
-  gen_server:call(Module, stop).
+-define(SERVER, ?MODULE). 
 
-stop() ->
-  stop(?MODULE).
+-record(state, {}).
 
-state(Module) ->
-  gen_server:call(Module, state).
+%%%===================================================================
+%%% API
+%%%===================================================================
 
-state() ->
-  state(?MODULE).
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-%% Server implementation, a.k.a.: callbacks
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
 init([]) ->
-  say("init", []),
-  {ok, []}.
+    {ok, #state{}}.
 
-handle_call(stop, _From, State) ->
-  say("stopping by ~p, state was ~p.", [_From, State]),
-  {stop, normal, stopped, State};
-
-handle_call(state, _From, State) ->
-  say("~p is asking for the state.", [_From]),
-  {reply, State, State};
-
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
-  say("call ~p, ~p, ~p.", [_Request, _From, State]),
-  {reply, ok, State}.
+    Reply = ok,
+    {reply, Reply, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
-  say("cast ~p, ~p.", [_Msg, State]),
-  {noreply, State}.
+    {noreply, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
 handle_info(_Info, State) ->
-  say("info ~p, ~p.", [_Info, State]),
-  {noreply, State}.
+    {noreply, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
-  say("terminate ~p, ~p", [_Reason, _State]),
-  ok.
+    ok.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
-  say("code_change ~p, ~p, ~p", [_OldVsn, State, _Extra]),
-  {ok, State}.
+    {ok, State}.
 
-%% Some helper methods.
-
-say(Format) ->
-  say(Format, []).
-say(Format, Data) ->
-  io:format("~p:~p: ~s~n", [?MODULE, self(), io_lib:format(Format, Data)]).
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
